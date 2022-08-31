654a72433e2a9bd61785d1351af64736d901bc09
ryanbuchananxyz <ryan@ryanbuchanan.xyz>
2022-08-31 13:34:17 +0100

    disallow duplicate titles, improve template appearance

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,25 +9,32 @@ def index(request):
         # pass form url
         url = request.POST.get('imdb_url')
         r = requests.get(url)
+
         # extract relevant data
         soup = BeautifulSoup(r.content, features='html.parser')
         data = json.loads(soup.find('script', type='application/ld+json').text)
+
         # save to database
         if data['@type'] == 'Movie':
-            new_movie = Movie(
-                title = data['name'],
-                poster = data['image'],
-                imdb_URL = 'https://www.imdb.com%s' % data['url']
-                )
-            new_movie.save()
+            if Movie.objects.filter(title=data['name']):
+                pass
+            else:
+                new_movie = Movie(
+                    title = data['name'],
+                    poster = data['image'],
+                    imdb_URL = 'https://www.imdb.com%s' % data['url']
+                    )
+                new_movie.save()
+
         elif data['@type'] == 'TVSeries':
-            new_series = Series(
-                title = data['name'],
-                poster = data['image'],
-                imdb_URL = 'https://www.imdb.com%s' % data['url']
-                )
-            new_series.save()
-    try:
-        return render(request, 'index.html', {'movies': Movie.objects.all(), 'tvseries': Series.objects.all()})
-    except:
-        return render(request, 'index.html')
+            if Series.objects.filter(title=data['name']):
+                pass
+            else:
+                new_series = Series(
+                    title = data['name'],
+                    poster = data['image'],
+                    imdb_URL = 'https://www.imdb.com%s' % data['url']
+                    )
+                new_series.save()
+
+    return render(request, 'index.html', {'movies': Movie.objects.all(), 'tvseries': Series.objects.all()})
